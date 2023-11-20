763153bca7c631a2c9b6ed40c559289162df0deb
SanskrutiUkey <110550398+SanskrutiUkey@users.noreply.github.com>
2023-11-20 12:44:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ import categoryRoute from './routes/categoryRoute.js'
 import productRoute from './routes/productRoute.js'
 import cors from 'cors'
 import path from 'path';
+import { fileURLToPath } from 'url';
 // configure env
 dotenv.config()
 
 // database connect
 connectDB();
-
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 // rest object
 const app = express()
 
